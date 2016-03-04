7894b083a36506109a58e45d5d7d6e6c659afdd1
Pavle Lee <523260513@qq.com>
2016-03-04 13:08:26 +0800

    更新sh

--- vagrant.sh
+++ vagrant.sh
@@ -5,28 +5,30 @@
 
 # update everything if not
 
-sudo apt-get update
+sudo su -
+
+apt-get update
 
 # ------- PPA's -------
 
 # utility to be able to use apt-add-repository
-sudo apt-get install -y python-software-properties
+apt-get install -y python-software-properties
 
 # we try to avoid running it by checking if the script didn't come from vagrant
 if [ ! -t 0 ]; then
 	# add ppa's
-	sudo add-apt-repository -y ppa:ondrej/php5-5.6
+	add-apt-repository -y ppa:ondrej/php5-5.6
 	# update list with new ppa's
-	sudo apt-get update
+	apt-get update
 fi
 
 # install required packages
-sudo apt-get install -y php5 php5-fpm php5-mysql php5-mongo php5-curl nginx curl
+apt-get install -y php5 php5-fpm php5-mysql php5-mongo php5-curl php5-mcrypt nginx curl
 
 # install composer
 if [ ! -f /usr/local/bin/composer ]; then
-	sudo curl -sS https://getcomposer.org/installer | php -- --install-dir=/usr/local/bin --filename=composer
-    composer global require fxp/composer-asset-plugin --prefer-dist
+	curl -sS https://getcomposer.org/installer | php -- --install-dir=/usr/local/bin --filename=composer
+    composer global require "fxp/composer-asset-plugin:~1.1.1"
 else
 	composer self-update
 	composer global update --prefer-dist
